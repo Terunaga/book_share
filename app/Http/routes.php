7782d34553bcbb1101dc52e33a6b01e9e1f0d661
田村光隆 <terunaga_tamura@hotmail.co.jp>
2017-04-22 23:16:08 +0900

    execute books migration

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -9,4 +9,5 @@ Route::group(['middleware' => ['web']], function () {
     Route::get('/', 'TopController@index');
     Route::resource('top', 'TopController', ['only' => 'index']);
     Route::resource('users', 'UsersController', ['only' => ['show', 'edit', 'update']]);
+    Route::resource('books', 'BooksController', ['only' => ['create', 'store', 'edit', 'update']]);
 });
