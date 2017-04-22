8fee92d19afcf50ea1a75055ab2fc4690c103f15
田村光隆 <terunaga_tamura@hotmail.co.jp>
2017-04-22 23:17:18 +0900

    implement a function to register book

---
+++ app/Http/Controllers/BooksController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Http\Requests;
+use App\Book;
+use Auth;
+
+class BooksController extends Controller
+{
+    public function __construct()
+    {
+      $this->middleware('auth', array('only' => 'create'));
+    }
+
+    public function create()
+    {
+      $book = new Book();
+
+      return view('books.create')->with('book', $book);
+    }
+
+    public function store(Request $request)
+    {
+      $user = Auth::user();
+      $user->books()->create(
+        array(
+          'name'    => $request->name,
+          'rate'    => $request->rate,
+          'image'   => $request->image,
+          'comment' => $request->name,
+        )
+      );
+      return redirect('/');
+    }
+}
