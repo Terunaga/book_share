390db5159b247657135fb32b35d31c4180461a3a
田村光隆 <terunaga_tamura@hotmail.co.jp>
2017-05-13 00:28:23 +0900

    set routings for book review

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -11,5 +11,6 @@ Route::group(['middleware' => ['web']], function () {
     Route::resource('users', 'UsersController', ['only' => ['show', 'edit', 'update']]);
     Route::resource('books', 'BooksController', ['only' => ['create', 'store', 'edit', 'update', 'show']]);
     Route::resource('books.borrows', 'Books\BorrowsController', ['only' => ['create', 'store']]);
+    Route::resource('books.reviews', 'Books\ReviewsController', ['only' => ['create', 'store']]);
     Route::get('books/{id}/reserves', 'Books\ReservesController@store');
 });
