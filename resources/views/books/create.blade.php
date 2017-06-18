c4e8397fb4715fcd607a0de3ce36bb9ffe76d05c
田村光隆 <terunaga_tamura@hotmail.co.jp>
2017-06-18 14:15:53 +0900

    display error messages

--- resources/views/books/create.blade.php
+++ resources/views/books/create.blade.php
@@ -2,14 +2,20 @@
 
 @section('content')
 <div class="row">
-  <div class="col-lg-12">
-    <h1 class="page-header">本を登録する</h1>
-
-    <div class="books">
-      {{ Form::model($book, array('action' => array('BooksController@store'))) }}
-        @include('books.form', ['submit_message' => '登録する', 'book' => $book])
-      {!! Form::close() !!}
+    <div class="col-lg-12">
+        <h1 class="page-header">本を登録する</h1>
+            @if (count($errors) > 0)
+            <ul>
+                @foreach ($errors->all() as $error)
+                    <li>{{ $error }}</li>
+                @endforeach
+            </ul>
+            @endif
+        <div class="books">
+          {{ Form::model($book, array('action' => array('BooksController@store'))) }}
+            @include('books.form', ['submit_message' => '登録する', 'book' => $book])
+          {!! Form::close() !!}
+        </div>
     </div>
-  </div>
 </div>
 @endsection
